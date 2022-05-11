ebd1db0453280f2dc05eb815c62e9808b9a3bc38
JFerrettUK <jferrettuk@gmail.com>
2022-05-11 08:47:19 +0100

    Concatenated strings added to RPS app html.

--- rps-app-v4.js
+++ rps-app-v4.js
@@ -40,34 +40,31 @@ function computerPlay() {
 //◦ If Paper > Paper
 //◦ If Scissors > Scissors
 
-let playerWinRound = "You win! ${playerSelectionUC} beats ${computerSelection}.";
-let computerWinRound = "Computer wins this round!";
-let draw = "Draw!";
-let playerWin = "Player wins the game! Congratulations!";
-let computerWin = "Computer wins the game! Congratulations!";
-
 function playRound() {
+    function titleCase(){
+        return playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+      }
+
     let playerSelection = prompt("Rock, paper, or scissors?");
-    let playerSelectionUC = playerSelection.toUpperCase();
+    let playerSelection2 = titleCase(playerSelection);
     let computerSelection = computerPlay();
-    if ((playerSelectionUC === "rock") && (computerSelection === "scissors")) {
-        return ("You win! Rock beats scissors.") && playerWinRound;
-    } else if ((playerSelectionUC === "scissors") && (computerSelection === "paper")) {
-        return ("You win! Scissors beats paper.") && playerWinRound; 
-    } else if ((playerSelectionUC === "paper") && (computerSelection === "rock")) {
-        return ("You win! Paper beats rock.") && playerWinRound; 
-    } else if ((playerSelectionUC === "paper") && (computerSelection === "scissors")) {
-        return ("You lose! Paper is beaten by scissors.") && computerWinRound; 
-    } else if ((playerSelectionUC === "rock") && (computerSelection === "paper")) {
-        return ("You lose! Rock is beaten by paper.") && computerWinRound; 
-    } else if ((playerSelectionUC === "scissors") && (computerSelection === "rock")) {
-        return ("You lose! Scissors are beaten by rock.") && computerWinRound; 
-    } else if ((playerSelectionUC === "paper") && (computerSelection === "paper")) {
-        return ("Paper versus paper is a draw...") && draw; 
-    } else if ((playerSelectionUC === "rock") && (computerSelection === "rock")) {
-        return ("Rock versus rock is a draw...") && draw; 
-    } else if ((playerSelectionUC === "scissors") && (computerSelection === "scissors")) {
-        return ("Scissors versus scissors is a draw") && draw; 
+
+    let playerWinRound = ("You win! " + playerSelection2 + " beats " + computerSelection + ".");
+    let computerWinRound = ("You lose! " + playerSelection2 + " is beaten by " + computerSelection + ".");
+    let draw = "Draw!";
+
+    if (((playerSelection2 === "Rock") && (computerSelection === "scissors")) 
+        || ((playerSelection2 === "Scissors") && (computerSelection === "paper"))
+        || ((playerSelection2 === "Paper") && (computerSelection === "rock"))) {
+        return playerWinRound;
+    } else if (((playerSelection2 === "Paper") && (computerSelection === "scissors"))
+        || ((playerSelection2 === "Rock") && (computerSelection === "paper")) 
+        || ((playerSelection2 === "Scissors") && (computerSelection === "rock"))) {
+        return computerWinRound; 
+    } else if (((playerSelection2 === "Paper") && (computerSelection === "paper"))
+        || ((playerSelection2 === "Rock") && (computerSelection === "rock"))
+        || ((playerSelection2 === "Scissors") && (computerSelection === "scissors"))) {
+        return draw; 
     } else {
         return ("What?")
     }
@@ -83,6 +80,9 @@ function playRound() {
 //Keep score 
 
 function game(playRound) {
+    let playerWin = "Player wins the game! Congratulations!";
+    let computerWin = "Computer wins the game! Congratulations!";
+    
     for (let i = 0; i < 5; i++) {
         console.log(playRound());
      }
