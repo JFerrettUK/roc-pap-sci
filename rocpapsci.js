66d6b7fab68316a2022e27f1c0e7d1f47ddf335f
JFerrettUK <jferrettuk@gmail.com>
2022-05-15 11:18:58 +0100

    JS cleanup of rocpapsci.

--- rocpapsci.js
+++ rocpapsci.js
@@ -1,10 +1,3 @@
-//Your game is going to play against the computer, 
-//so begin with a function called computerPlay that
-//will randomly return either ‘Rock’, ‘Paper’ or 
-//‘Scissors’. We’ll use this function in the game 
-//to make the computer’s play. Tip: use the console 
-//to make sure this is returning the expected output 
-//before moving to the next step!
 
 //A function called ComputerPlay which randomly returns either Rock, Paper or Scissors.
 
